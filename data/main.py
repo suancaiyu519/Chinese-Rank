50ea3af7c542a77badd945f68aacd812d8769ddc
suancaiyu <suancaiyu1130@gmail.com>
2024-08-01 10:03:04 +0800

    Update main.py

--- data/main.py
+++ data/main.py
@@ -3,32 +3,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import CONFIG
 from data.models import Base
-import schedule
-import time
+from log_config import logger
 
 # 创建数据库引擎
 engine = create_engine(CONFIG.SQLALCHEMY_DATABASE_URI)
 Session = sessionmaker(bind=engine)
 
+# 收集时间区间
 start = CONFIG.start
 end = CONFIG.end
 
-
-def job():
+if __name__ == '__main__':
     session = Session()
+    logger.info("Github 国内项目收集开始!")
     try:
         Base.metadata.create_all(engine)
         data_collect(start, end, session)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
     finally:
+        logger.info("Github 国内项目收集完成!")
         session.close()
-
-
-schedule.every(CONFIG.cycles).days.do(job)
-
-if __name__ == '__main__':
-    job()
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
